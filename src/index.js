3faa80de97802bbdb0ddf3a593eaf6749166d7ff
Nikita_Kurochka <freenikita95@gmail.com>
2020-02-18 23:55:08 +0200

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Routes from 'router';
-import AppBar from 'components/appBar';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "router";
+import AppBar from "components/appBar";
 
 const App = () => {
   return (
@@ -13,4 +13,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
