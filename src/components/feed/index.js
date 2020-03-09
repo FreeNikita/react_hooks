117cf837f8ef7f1b27903131d584414c49985bc7
FreeNikita <freenikita95@gmail.com>
2020-03-09 16:35:47 +0200

    refactoring

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -5,30 +5,29 @@ import { PAGE_PROFILE_URL, PAGE_ARTICLE_URL } from "constants/router";
 const Feed = ({articles}) => {
     return (
         <div>
-            {articles.map((article, index) => (
+            {articles.map( ({author: {username, image}, name, description, createdAt, slug, tagList }, index) => (
                 <div className="article-preview" key={index}>
                     <div className='article-meta'>
-                        <Link to={`${PAGE_PROFILE_URL}${article.author.username}`}>
-                            <img src={article.author.image} alt=''/>
+                        <Link to={`${PAGE_PROFILE_URL}${username}`}>
+                            <img src={image} alt=''/>
                         </Link>
                     </div>
                     <div className='info'>
-                        <Link to={`${PAGE_PROFILE_URL}${article.author.username}`} className='author'>
-                            {article.author.username}
+                        <Link to={`${PAGE_PROFILE_URL}${username}`} className='author'>
+                            {username}
                         </Link>
                     </div>
-                    <span className='date'>{article.createdAt}</span>
-                    <Link to={`${PAGE_ARTICLE_URL}${article.slug}`} className='preview-link'>
-                        <h1>{article.name}</h1>
-                        <p>{article.description}</p>
+                    <span className='date'>{createdAt}</span>
+                    <Link to={`${PAGE_ARTICLE_URL}${slug}`} className='preview-link'>
+                        <h1>{name}</h1>
+                        <p>{description}</p>
                         <span>Read more...</span>
                         <ul className='tag-list'>
-                            {article.tagList.map((tag)=> (
+                            {tagList.map((tag)=> (
                               <li className='tag-pill tag-default tag-outline' key={tag}>
                                   {tag}
                               </li>
                             ))}
-
                         </ul>
                     </Link>
                 </div>
