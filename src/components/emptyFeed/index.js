27ef0f4afd829df72e4e3c10e6a4c1676499ae74
FreeNikita <freenikita95@gmail.com>
2020-03-14 23:33:14 +0200

    add message if listFeed is empty

---
+++ src/components/emptyFeed/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EmptyFeed = () => <div className="article-preview">No articles are here... yet.</div>;
+
+export default EmptyFeed;
--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -3,42 +3,50 @@ import { Link } from 'react-router-dom';
 import { PAGE_PROFILE_URL, PAGE_ARTICLE_URL } from 'constants/router';
 import TagList from 'components/tagList';
 import AddToFavorite from 'components/addToFavorite';
+import EmptyFeed from 'components/emptyFeed';
 
-const Feed = ({ articles }) => (
-  <div>
-    {articles.map(({
-      author: { username, image }, name, description, createdAt, slug, tagList, favorited,
-      favoritesCount,
-    }) => (
-      <div className="article-preview" key={createdAt}>
-        <div className="article-meta">
-          <Link to={`${PAGE_PROFILE_URL}/${username}`}>
-            <img src={image} alt="" />
-          </Link>
+const Feed = ({ articles }) => {
+  if (articles.length === 0) {
+    return <EmptyFeed />;
+  }
+  return (
+    (
+      <div>
+        {articles.map(({
+          author: { username, image }, name, description, createdAt, slug, tagList, favorited,
+          favoritesCount,
+        }) => (
+          <div className="article-preview" key={createdAt}>
+            <div className="article-meta">
+              <Link to={`${PAGE_PROFILE_URL}/${username}`}>
+                <img src={image} alt="" />
+              </Link>
 
-          <div className="info">
-            <Link to={`${PAGE_PROFILE_URL}/${username}`} className="author">
-              {username}
+              <div className="info">
+                <Link to={`${PAGE_PROFILE_URL}/${username}`} className="author">
+                  {username}
+                </Link>
+              </div>
+              <div className="pull-xs-right">
+                <AddToFavorite
+                  isFavorite={favorited}
+                  articleSlug={slug}
+                  favoritesCount={favoritesCount}
+                />
+              </div>
+            </div>
+            <span className="date">{createdAt}</span>
+            <Link to={`${PAGE_ARTICLE_URL}/${slug}`} className="preview-link">
+              <h1>{name}</h1>
+              <p>{description}</p>
+              <span>Read more...</span>
+              <TagList tagList={tagList} />
             </Link>
           </div>
-          <div className="pull-xs-right">
-            <AddToFavorite
-              isFavorite={favorited}
-              articleSlug={slug}
-              favoritesCount={favoritesCount}
-            />
-          </div>
-        </div>
-        <span className="date">{createdAt}</span>
-        <Link to={`${PAGE_ARTICLE_URL}/${slug}`} className="preview-link">
-          <h1>{name}</h1>
-          <p>{description}</p>
-          <span>Read more...</span>
-          <TagList tagList={tagList} />
-        </Link>
+        ))}
       </div>
-    ))}
-  </div>
-);
+    )
+  );
+};
 
 export default Feed;
